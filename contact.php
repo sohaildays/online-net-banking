5026b714dbe71c53698dd6ff60c5adfc32de8f12
sohaildays <84544753+sohaildays@users.noreply.github.com>
2021-05-21 03:46:38 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,65 @@
+<?php
+    include "header.php";
+    include "navbar.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="contact_style.css">
+</head>
+
+<body>
+    <div class="flex-container-background">
+        <div class="flex-container-heading">
+            <h1 id="contact">Contact Us</h1>
+        </div>
+
+        <div class="flex-container" style="border-bottom: 0;
+                                           border-top-left-radius: 10px;
+                                           border-top-right-radius: 10px;">
+            <div class="flex-item">
+                <h1 id="sub-contact">SAKEC Corporate Headquarters</h1>
+                <p id="sub-contact">
+                    Corporate HQ<br>SAKEC Bank<br>
+                    W.T Patil Marg, Chembur, next to Duke's Company<br>
+                    Mumbai, Maharashtra 400 088
+                </p>
+            </div>
+            <div class="flex-item">
+                <h1 id="sub-contact">General Contact</h1>
+                <p id="sub-contact">
+                    Toll-Free: 111-222-3333<br>
+                    Phone: 444-555-6666<br>
+                    Fax: 777-888-9999<br>
+                    Email: office@&#8203sakecbank.com
+                </p>
+            </div>
+        </div>
+
+        <div class="flex-container" style="border-top: 0;
+                                           border-bottom-left-radius: 10px;
+                                           border-bottom-right-radius: 10px;">
+            <div class="flex-item">
+                <h1 id="sub-contact">Customer Care (24x7)</h1>
+                <p id="sub-contact">
+                    Toll-Free: 888-966-6992<br>
+                    Phone: 732-666-5555<br>
+                    Email: care@&#8203sakecbank.com
+                </p>
+            </div>
+            <div class="flex-item">
+                <h1 id="sub-contact">Live Chat</h1>
+                <p id="sub-contact">
+                    Download our app and live chat<br>
+                    with our customer care !<br>
+                    App available on Google Play<br>
+                    and iPhone-AppStore.
+                </p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
